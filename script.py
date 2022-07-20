3e5e4e8b8a87c042305fbb3ec5a7c2bfff4a4edd
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2022-07-20 03:54:52 +0200

    update script and config

--- script.py
+++ script.py
@@ -1,9 +1,5 @@
 from adapter import BioCypherAdapter
 
-adapter = BioCypherAdapter(db_name="small")
-
-# adapter.write_nodes()
-# adapter.write_edges()
-# adapter.bcy.write_import_call()
+adapter = BioCypherAdapter(db_name="large")
 
 adapter.write_to_csv_for_admin_import()
